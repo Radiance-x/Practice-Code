a7c4ee04c96d2a0cf06896b16278716db5501acd
Radiance-x <cyh363713@yeah.net>
2024-06-29 12:09:12 +0800

    test_6_29(2)

--- test_6_29/test_6_29/源.c
+++ test_6_29/test_6_29/源.c
@@ -1,12 +1,41 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int t = 0;
+//	int w = 0;
+//	int sum = 0;
+//	scanf("%d %d", &t, &w);
+//	sum = t + w;
+//	printf("sum =%d", sum);
+//	return 0;
+//}
+
+//#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	float a = 0;
+//	float b = 0;
+//	float c = 0;
+//	scanf("%f %f", &a, &b);
+//	c = a + b;
+//    printf("a+b=%f\n", c);
+//	return 0;
+//}
+
+#define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 int main()
 {
-	int t = 0;
-	int w = 0;
-	int sum = 0;
-	scanf("%d %d", &t, &w);
-	sum = t + w;
-	printf("sum =%d", sum);
+	double x = 0;
+	double y = 0;
+	double z = 0;
+	double sum = 0;
+
+	scanf("%lf %lf %lf", &x, &y, &z);
+	sum = x + y + z;
+	printf("三个数之和=%lf", sum);
+
 	return 0;
 }
