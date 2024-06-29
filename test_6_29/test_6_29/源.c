d6947f2ccb93e72f95b2f59bd405ffba4bfc29cb
Radiance-x <cyh363713@yeah.net>
2024-06-29 09:53:09 +0800

    Add files via upload

---
+++ test_6_29/test_6_29/源.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int t = 0;
+	int w = 0;
+	int sum = 0;
+	scanf("%d %d", &t, &w);
+	sum = t + w;
+	printf("sum =%d", sum);
+	return 0;
+}
