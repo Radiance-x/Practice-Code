a7e5e7e208a0607c15d77805dcde2512f376d5dc
Radiance-x <cyh363713@yeah.net>
2024-07-03 11:57:54 +0800

    7.3

---
+++ test_7_3/test_7_3/源.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//
+//	scanf("%d %d", &a, &b);
+//	c = a * b;
+//	printf("»ý=%d", c);
+//
+//	return 0;
+//}
+
+
+enum TYPE
+{
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G
+};
+int main()
+{
+    
+	printf("%d\n",F);
+
+    return 0;
+}
+
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRET
+//};
+//int main()
+//{
+//	enum Sex s = MALE;
+//	printf("%d\n", MALE);
+//
+//
+//
+//	return 0;
+//}
